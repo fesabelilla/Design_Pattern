61c20ac00211d7d169725850ffea31d5bbaf1f12
fesabelilla <fesabelilla@gmail.com>
2023-05-23 21:18:19 +0600

    Add Res In The Controller | Fall Back Implementation Done

--- circuit-breakers-using-resilience4J/src/main/java/com/example/circuitbreakersusingresilience4J/controller/ActivityController.java
+++ circuit-breakers-using-resilience4J/src/main/java/com/example/circuitbreakersusingresilience4J/controller/ActivityController.java
@@ -1,11 +1,19 @@
 package com.example.circuitbreakersusingresilience4J.controller;
 
 import com.example.circuitbreakersusingresilience4J.model.Activity;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+/*
+Resilience4j : https://resilience4j.readme.io/docs/circuitbreaker
+Tutorial : https://www.youtube.com/watch?v=3-ChrD3Zosg
+For Health Check : http://localhost:8080/actuator/health
+ */
+
 
 @RestController
 @RequestMapping("/activity")
@@ -19,11 +27,16 @@ public class ActivityController {
     }
 
     @GetMapping
+    @CircuitBreaker(name = "randomActivity", fallbackMethod = "fallbackRandomActivity")
     public String getRandomActivity() {
         ResponseEntity<Activity> responseEntity = restTemplate.getForEntity(BORED_API, Activity.class);
         Activity activity = responseEntity.getBody();
 
         System.out.println("Activity Received: " + activity.getActivity());
         return activity.getActivity();
+    }
+
+    public String fallbackRandomActivity(Throwable throwable) {
+        return "Fallback Random Activity";
     }
 }
